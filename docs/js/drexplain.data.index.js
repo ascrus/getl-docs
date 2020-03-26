cc55b8b58d7a4eaecbc9d0fbd37c23423ad95b4b
Serdobintceva Olga <olga@easydata.ru>
2020-03-26 14:57:21 +0300

    pics

--- docs/js/drexplain.data.index.js
+++ docs/js/drexplain.data.index.js
@@ -3,8 +3,8 @@ DR_EXPLAIN.data_index = {
 
 	// index
 	DREX_NODE_KEYWORDS: [],
-	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], //length:= drex.nodes_count,
-	DREX_NODE_KEYWORDS_END: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_START: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], //length:= drex.nodes_count,
 
 	DREX_KEYWORD_NAMES: ["<НОВОЕ КЛЮЧЕВОЕ СЛОВО>","connection","dataset","filemanager","репозиторий","имя объекта","группа объекта","forGroup","соединение","конфигурация","набор данных","менеджер файлов","регистрация","умолчанию","файл"],
 	DREX_KEYWORD_CHILD_START: [1,12,12,12,12,13,13,13,13,14,15,15,15,15,15],
